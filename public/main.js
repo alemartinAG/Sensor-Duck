dbd1128108f6a42d8815e5de3c270fa6dca2da82
alemartinAG <ale.martin96@hotmail.com>
2018-10-23 11:06:29 -0300

    uploading existing project

---
+++ public/main.js
@@ -0,0 +1,228 @@
+var socket = io.connect('http://localhost:8080', {'forceNew' : true});
+
+socket.on('messages', function(data){
+	console.log(data);
+});
+
+var canvas = document.getElementById("canvas");
+
+var context = canvas.getContext("2d");
+context.font = 'bold 25pt Arial';
+
+///
+var bird = new Image();
+var bird_2 = new Image();
+var background = new Image();
+var pipe_up = new Image();
+var pipe_dw = new Image();
+
+bird.src = "images/bird.png";
+bird_2.src = "images/bird2.png";
+background.src = "images/background.png";
+pipe_up.src = "images/obstaculo.png";
+pipe_dw.src = "images/obstaculo2.png";
+///
+
+///
+var scorebg = new Image();
+scorebg.src = "images/score.png";
+///
+
+///
+var quack = new Audio();
+quack.src = "sounds/quack.mp3";
+
+var song = new Audio();
+song.src = "sounds/song.mp3";
+///
+
+///
+var pausa = 1;
+var pausa_on = new Audio();
+var pausa_off = new Audio();
+pausa_on.src = "sounds/pausa.mp3";
+pausa_off.src = "sounds/pausaoff.wav";
+///
+
+///
+var x_pos = 100;
+var y_pos = 150;
+var scale = 0.9;
+
+var gravity = 2;
+var velocity = 2;
+
+var gap = 200+(10*velocity);
+var constant;
+///
+
+var pipe = [];
+
+pipe[0] = {
+    x : canvas.width,
+    y : 0,
+    flag_n : 0,
+    flag_s : 0
+};
+
+var score = 0;
+var highscores = [0, 0, 0];
+
+document.addEventListener("keydown", press);
+document.addEventListener("keyup", release);
+
+alert("Play");
+
+draw();
+
+function press(event){
+
+    //const keyName = event.key;
+    let keyName = event.key;
+
+    if(keyName == 'p'){
+        pausa = !pausa;
+
+        if(pausa){
+            pausa_off.play();
+        }else{
+            pausa_on.play();
+        }
+
+    }
+
+    if(keyName == 'a'){
+        y_pos -= 20*velocity*pausa;
+    }
+
+    if(keyName == 's'){
+        y_pos += 20*velocity*pausa;
+    }
+
+    if(y_pos < 0){
+        y_pos = 0;
+    }
+
+    if(y_pos > canvas.height-bird.height*scale){
+        y_pos = canvas.height-bird.height*scale;
+    }
+
+    bird.src = "images/bird2.png";
+}
+
+function release(){
+    bird.src = "images/bird.png";
+}
+
+function draw(){
+
+    context.drawImage(background, 0, 0);
+    context.drawImage(bird, x_pos, y_pos, bird.width*scale, bird.height*scale);
+
+    //song.play();
+
+    for(var i=0; i<pipe.length; i++){
+
+        constant = pipe_up.height+gap;
+
+        context.drawImage(pipe_dw, pipe[i].x, pipe[i].y);
+        context.drawImage(pipe_up, pipe[i].x, pipe[i].y+constant);
+
+        pipe[i].x -= 5*velocity*pausa;
+
+        if( pipe[i].x <= canvas.width/3 && !pipe[i].flag_n){
+
+            pipe[i].flag_n = 1;
+
+            pipe.push({
+                x : canvas.width,
+                y : Math.floor(Math.random()*pipe_up.height) - pipe_up.height,
+                flag_n : 0,
+                flag_s : 0
+            });
+        }
+
+        incrementScore(i);
+
+        checkCollision(i);
+    }
+
+    context.fillText("score: "+score,5,30);
+
+    //Tabla de puntajes
+    if(!pausa){
+        context.drawImage(scorebg, canvas.width/2-scorebg.width, canvas.height/2-scorebg.height/2);
+        for(let j=highscores.length-1; j>=0; j--){
+        	context.fillText(highscores[j], canvas.width/2, canvas.height/2 - j*50);
+        }
+    }
+
+    requestAnimationFrame(draw);
+}
+
+function checkCollision(i){
+
+    if(x_pos + (bird.width*scale) >= pipe[i].x && x_pos <= pipe[i].x + pipe_up.width && (y_pos <= pipe[i].y + pipe_up.height || y_pos+ (bird.height*scale) >= pipe[i].y+constant)){
+
+        for(var j=pipe.length-1; j>0; j--){
+            pipe.pop();
+        }
+
+        pipe[0] = {
+            x : canvas.width,
+            y : 0,
+            flag_n : 0,
+            flag_s : 0
+        };
+
+        compareScores();
+        sortScores();
+
+        score = 0;
+
+        x_pos = 100;
+        y_pos = 150;
+
+
+        //envio los puntajes
+        var payload = {
+        	first : highscores[2],
+        	second : highscores[1],
+        	third : highscores[0]
+        };
+
+        socket.emit('score-message', payload);
+    }
+}
+
+function incrementScore(i){
+
+    if(pipe[i].x <= 5 && !pipe[i].flag_s){
+        score++;
+        pipe[i].flag_s = 1;
+
+        quack.play();
+    }
+}
+
+function sortScores(){
+
+    var aux;
+
+    for(var j=0; j<highscores.length-1; j++){
+        if(highscores[j] > highscores[j+1]){
+            aux = highscores[j];
+            highscores[j] = highscores[j+1];
+            highscores[j+1] = aux;
+        }
+    }
+}
+
+function compareScores(){
+    for(var j=0; j<highscores.length; j++){
+        if(score > highscores[j]){
+            highscores[j] = score;
+            break;
+        }
+    }
+}
---
+++ server/main.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var SerialPort = require('serialport');
+var serialPort = SerialPort.serialPort;
+
+//unhandledRejection handler
+process.on('unhandledRejection', error => {
+  console.log("Problemas de conexion serie (no se pudo encontrar el puerto)");
+});
+
+// Open the port
+var port = new SerialPort("/dev/ttyUSB1", { baudRate: 9600 });
+
+
+app.use(express.static('public'));
+
+// Read the port data
+port.on("open", function () {
+    console.log('open');
+
+    try{
+    	port.on('data', function(data) {
+        	console.log(data);
+    	});
+    }
+    catch(UnhandledPromiseRejectionWarning){
+    	console.log("\nNo se pudo leer el puerto");
+    }
+});
+
+///
+
+app.get('/', function(req, res){
+	res.status(200).send("Ale");
+});
+
+io.on('connection', function(socket){
+	console.log('\nClient connected to the server\n');
+	
+	/*socket.emit('messages', {
+		id: 1,
+		text: "mensaje",
+		author: "ale"
+	});*/
+
+	socket.on('score-message', function(data){
+		//console.log(data.first);
+		try{
+			port.write(data.first);
+		}
+		catch(error){
+			//console.error(error);
+			console.log("\nNo hay conexion con serial port")
+		}
+	});
+});
+
+
+server.listen(8080, function(){
+	console.log("\nSv running in http://localhost:8080\n");
+});
+
